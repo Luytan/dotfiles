2b89911759445dff6eb261dec9fe2306e3bb682e
Luytan <luytan@khora.me>
2025-12-04 18:30:09 +0100

    bluetoot

---
+++ modules/common/bluetooth.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+{
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Experimental = true;
+        FastConnectable = true;
+      };
+      Policy = {
+        AutoEnable = true;
+      };
+    };
+  };
+}
