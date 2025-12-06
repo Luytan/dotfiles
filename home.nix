763127867b164b1297afa067d198daa89dd846fc
Luytan <luytan@khora.me>
2025-12-06 20:06:27 +0100

    nix lsp

--- home.nix
+++ home.nix
@@ -41,6 +41,8 @@
     filen-desktop
     neovim
     nixfmt
+    nil
+    nixd
   ];
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
