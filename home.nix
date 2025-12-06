aafde4d56e477b83a0b0861abbc15d4539120cd1
Luytan <luytan@khora.me>
2025-12-06 20:01:40 +0100

    zed

--- home.nix
+++ home.nix
@@ -9,20 +9,8 @@
     ./modules/cli/git.nix
     ./modules/cli/gpg.nix
     ./modules/cli/zoxide.nix
+    ./modules/editors/zed.nix
   ];
-  programs.zed-editor = {
-  enable = true;
-  extensions = [ "nix" "toml" "rust" ];
-  userSettings = {
-    theme = {
-      mode = "system";
-      dark = "One Dark";
-      light = "One Light";
-    };
-    hour_format = "hour24";
-    vim_mode = true;
-  };
-};
   #programs.ssh = {
   #  enable = true;
   #  extraConfig = ''
@@ -54,7 +42,6 @@
     neovim
     nixfmt
   ];
-
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
